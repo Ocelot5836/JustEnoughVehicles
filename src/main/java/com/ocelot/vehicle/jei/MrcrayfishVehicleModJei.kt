5e4ef53ae8f171ddf3cf8a5352c41884865c9124
Ocelot5836 <FinntheRaider@gmail.com>
2019-07-13 17:19:19 -0700

    Initial Commit

---
+++ src/main/java/com/ocelot/vehicle/jei/MrcrayfishVehicleModJei.kt
@@ -0,0 +1,10 @@
+package com.ocelot.vehicle.jei
+
+import net.minecraftforge.fml.common.Mod
+
+const val MOD_ID = "cvmjei"
+const val NAME = "MrCrayfish's Vehicle Mod JEI"
+const val VERSION = "1.0"
+
+@Mod(modid = MOD_ID, name = NAME, version = VERSION, acceptedMinecraftVersions = "[1.12,1.12.2]", dependencies = "required-after:vehicle@[0.41.0,)", clientSideOnly = true)
+class MrcrayfishVehicleModJei

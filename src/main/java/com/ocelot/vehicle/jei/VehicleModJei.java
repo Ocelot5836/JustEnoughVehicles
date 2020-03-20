30cd4a39e7344461425f22c2dd75a24537dae263
Ocelot5836 <FinntheRaider@gmail.com>
2020-03-20 03:43:41 -0700

    Updated to v1.1

--- src/main/java/com/ocelot/vehicle/jei/VehicleModJei.java
+++ src/main/java/com/ocelot/vehicle/jei/VehicleModJei.java
@@ -5,12 +5,12 @@ import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = VehicleModJei.MOD_ID, name = VehicleModJei.NAME, version = VehicleModJei.VERSION, acceptedMinecraftVersions = "[1.12,1.12.2]", dependencies = "required-after:vehicle@[0.41.0,);required-after:jei")
+@Mod(modid = VehicleModJei.MOD_ID, name = VehicleModJei.NAME, version = VehicleModJei.VERSION, acceptedMinecraftVersions = "[1.12,1.12.2]", dependencies = "required-after:vehicle@[0.43.0,);required-after:jei")
 public class VehicleModJei {
 	public static final String MOD_ID = "jev";
 	public static final String VEHICLE_NAME = "MrCrayfish's Vehicle Mod";
 	public static final String NAME = "Just Enough Vehicles";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 
 	public static final String FLUID_EXTRACTOR_UID = MOD_ID + ":fluid_extractor";
 	public static final String FLUID_MIXER_UID = MOD_ID + ":fluid_mixer";
